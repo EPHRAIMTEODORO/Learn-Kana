ff5a284f71f5925fdc4c710b64056c2245bfc318
Ephraim Teodoro <ephraimteodoro@gmail.com>
2026-05-03 00:08:00 -1000

    Fix npm audit vulnerabilities

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  outputFileTracingRoot: __dirname,
+};
 
 export default nextConfig;
